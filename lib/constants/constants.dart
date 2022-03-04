3e08e97dcd4c975974d40a7c6c1e6097d2b420f5
Binit <binitkoirala17@gmail.com>
2022-03-04 08:02:37 +0545

    all files with login and register

---
+++ lib/constants/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:rent_app/utils/size_config.dart';
+
+var basePadding = EdgeInsets.symmetric(
+  vertical: SizeConfig.height,
+  horizontal: SizeConfig.width * 4,
+);
+
+class ImageConstants {
+  static const _basePath = "assets/images";
+  static const logo = "$_basePath/logo.png";
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:rent_app/screens/login_screen.dart';
+import 'package:rent_app/theme/theme_data.dart';
+import 'package:rent_app/utils/size_config.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      SizeConfig().init(constraints);
+      return MaterialApp(
+        title: 'Flutter Demo',
+        theme: lightTheme(context),
+        home: LoginScreen(),
+      );
+    });
+  }
+}
